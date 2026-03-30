e93d9e5c65740a44268d06e6ec2e53476054a3e8
Alan Quillin <alanquillin@gmail.com>
2026-03-30 14:49:15 -0400

    fix linting error

--- api/db/audit.py
+++ api/db/audit.py
@@ -24,9 +24,7 @@ class DataChanges(Base, DictifiableMixin, AuditedMixin, QueryMethodsMixin):
 
 @event.listens_for(Base.metadata, "before_create")
 def create_audit_trigger(_target, connection, **_):
-    connection.execute(
-        text(
-            f"""
+    connection.execute(text(f"""
 CREATE OR REPLACE FUNCTION {FUNC_NAME}() RETURNS trigger AS $$
     BEGIN
         IF TG_OP = 'INSERT'
@@ -47,9 +45,7 @@ CREATE OR REPLACE FUNCTION {FUNC_NAME}() RETURNS trigger AS $$
         END IF;
     END;
 $$ LANGUAGE 'plpgsql'
-"""
-        )
-    )
+"""))
 
 
 @event.listens_for(Base.metadata, "after_drop")
